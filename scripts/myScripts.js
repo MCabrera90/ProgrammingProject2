aa348f2397884786871d7d6d6ba19b5e44e2db5e
Matheo Cabrera <mcabre24@stu.southtexascollege.edu>
2024-09-06 17:56:18 -0500

    Initial commit of Programming Project 2 assignment.

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -1,28 +1,48 @@
-const newName = document.getElementById("newNameTxt");
-const newNum = document.getElementById("newNumTxt");
-const addButton = document.getElementById("newContactBtn");
-const pendingList = document.getElementById("contactList");
-
-addButton.addEventListener("click", addItemFunc);
-function addItemFunc(){
+/*
+Programming Project 2
+Scripts
     
-    if(newName!="" && newNum!=""){
-        const addItem = document.createElement("li")
+Author: Matheo Andres Cabrera 
+Date: 9/3/24
+
+Filename: myScripts.js
+*/ 
+
+//declare variables for contact name, contact number, button, and contact list
+const newName = document.getElementById("newNameTxt"); //contact name user input
+const newNum = document.getElementById("newNumTxt"); //contact number user input
+const addContactBtn = document.getElementById("newContactBtn"); //button
+const contactList = document.getElementById("contactList"); //contact list
+
+//event handlers
+addContactBtn.addEventListener("click", addContact);
+contactList.addEventListener("click", function(){ removeContact(event)});
+
+//function to add item from user input into list
+function addContact(){
+    if (newName.value != "" && newNum.value != "")   //verify that user input is not blank
+    {
+        const addItem = document.createElement("li") //create list item
+        //set value of list item to contact name and contact number
         addItem.innerHTML = newName.value + " - " + newNum.value
-        pendingList.appendChild(addItem)
+        contactList.appendChild(addItem) //add input to list
+        //remove text from textboxes
         newName.value = ""
         newNum.value = ""
     }
-    else {
+    else
+    {
+        //error message if input is invalid/blank
         alert("Input a name and number to add to the Contact List.")
     }
 }
-
-pendingList.addEventListener("click", function(){ removeContact(event)})
-function removeContact(event){
-    if(event.target.localName == "li"){
-        const item = event.target
-        item.remove();
+function removeContact(event)
+{
+    //This function removes a contact from the contact list
+    if(event.target.localName == "li") //check if target is a list item
+    {
+        const item = event.target //isolate list item
+        item.remove(); //remove list item
     }
 }
 
